ffb0c2976cf1896a2c1d97427d9932301c61d6ac
MBatzdorf <mbatzdorf@MBatzdorf.de>
2017-06-13 11:00:52 +0200

    Finished commenting

--- pointing_experiment.py
+++ pointing_experiment.py
@@ -8,11 +8,12 @@ import itertools
 import json
 import configparser
 import random
-# Did not work for me: Error module not found
-#import ITT_Assignment_5.pointing_technique as pt
+# import ITT_Assignment_5.pointing_technique as pt
 import pointing_technique as pt
-# import pointing_technique as pt
 from PyQt5 import QtGui, QtWidgets, QtCore
+
+# This script was created by Alexander Frummet and Marco Batzdorf
+# and is based on the "fitts_law_test.py" script
 
 """ setup ini file format:
 [experiment_setup]
@@ -35,9 +36,17 @@ ImprovePointing = 0
 class PointingExperimentModel(object):
 
     """
+        This experiment model keeps track of all information concerning a pointing test.
+        The experiment settings are given via instantiation parameters and the trial order is generated in a counterbalanced way
+        Further all information are logged to stdout and stored in a file in csv format.
 
+        @param user_id: ID for the user participating
+        @param diameters: A List containing all possible target diameters for this test
+        @param distances: A List containing all possible distances between pointer and target for this test
+        @param improve_pointing: Whether the improved pointing technique should be used or the standard one
+        @param repetitions: Indicates how often all trials should be repeated
     """
-    
+
     def __init__(self, user_id, diameters, distances, improve_pointing, repetitions=4):
         self.timer = QtCore.QTime()
         self.user_id = user_id
@@ -55,6 +64,7 @@ class PointingExperimentModel(object):
             "timestamp (ISO); user_id; trial; target_distance; target_size; movement_time (ms); click_offset_x; "
             "click_offset_y; number_of_errors; improved_pointing")
 
+    ''' Initializes the order of the single trials '''
     def init_trials(self, distances, diameters, repetitions):
         # gives us a list of (distance, width) tuples:
         self.trials = repetitions * list(itertools.product(distances, diameters))
@@ -63,6 +73,7 @@ class PointingExperimentModel(object):
         for i in range(len(self.trials)):
             self.trials[i] = Trial(self.trials[i][0], self.trials[i][1])
 
+    ''' Counterbalances a given list of [distance, diameter] tuples '''
     def counterbalance_trials(self, trials, userid):
         n = len(trials)
         # https://stackoverflow.com/questions/6667201/how-to-define-two-dimensional-array-in-python
@@ -76,6 +87,9 @@ class PointingExperimentModel(object):
 
         return balanced_trials_matrix[int(userid) - 1]
 
+    ''' Initialize experiment logging
+        Creates a new csv file and writes the corresponding header line
+    '''
     def init_logging(self):
         self.logfile = open("user" + str(self.user_id) + ".csv", "a")
         self.out = csv.DictWriter(self.logfile,
@@ -84,20 +98,25 @@ class PointingExperimentModel(object):
                                    "number_of_errors", "improved_pointing"], delimiter=";", quoting=csv.QUOTE_ALL)
         self.out.writeheader()
 
+    ''' Helper for getting the current [distance, diameter] tuple representing the current trial's settings'''
     def current_trial(self):
         if self.elapsed >= len(self.trials):
             return None
         else:
             return self.trials[self.elapsed]
 
+    ''' Tells the model that the correct target has been hit
+        Updates the current target to the next one and triggers writing the trial results to the csv file
+    '''
     def register_click(self, target_pos, click_pos):
         click_offset = (target_pos[0] - click_pos[0], target_pos[1] - click_pos[1])
         self.log_time(self.stop_measurement(), click_offset)
         self.errors = 0
         self.elapsed += 1
 
+    ''' Writes all useful trial information to the log csv file'''
     def log_time(self, time, click_offset):
-        distance, diameters = self.current_trial().getCurrentCondition()
+        distance, diameters = self.current_trial().get_current_condition()
         current_values = {"timestamp (ISO)": self.timestamp(), "user_id": self.user_id,
                           "trial": self.elapsed, "target_distance": distance,
                           "target_size": diameters, "movement_time (ms)": time,
@@ -109,11 +128,15 @@ class PointingExperimentModel(object):
             self.timestamp(), self.user_id, self.elapsed, distance, diameters, time, click_offset[0], click_offset[1],
             self.errors, self.improve_pointing))
 
+    ''' Tells the model to start the timer and that the mouse is moving '''
     def start_measurement(self):
         if not self.mouse_moving:
             self.timer.start()
             self.mouse_moving = True
 
+    ''' Stops counting the time because the mouse is not moving anymore.
+        Returns the elapsed time for the current trial
+    '''
     def stop_measurement(self):
         if self.mouse_moving:
             elapsed = self.timer.elapsed()
@@ -122,23 +145,48 @@ class PointingExperimentModel(object):
         else:
             return -1
 
+    ''' Returns a timestamp'''
     def timestamp(self):
         return QtCore.QDateTime.currentDateTime().toString(QtCore.Qt.ISODate)
 
+    ''' Helper to increment the error count for the current trial'''
     def increment_error_count(self, amount):
         self.errors += amount
 
 
 class Trial:
+
+    """
+        Stores the settings for a single trial of the experiment
+
+        @param distance: The distance between target and cursor
+        @param diameter: The size of the target to hit
+    """
+
     def __init__(self, distance, diameter):
         self.distance = distance
         self.diameter = diameter
 
-    def getCurrentCondition(self):
+    ''' Helper for accessing this trial's settings'''
+    def get_current_condition(self):
         return self.distance, self.diameter
 
 
 class Target:
+
+    """
+        Represents a target that can be displayed on the participant's screen
+        Targets are represented by a circle and can be highlighted
+        The default color is white but can be given any color
+
+        @param position_x: X-Coordinate of the circle's center
+        @param position_y: Y-Coordinate of the circle's center
+        @param diameter: Two times the radius, defining the circle's extends
+    """
+
+    COLOR_RED = QtGui.QColor(200, 34, 20)
+    COLOR_WHITE = QtGui.QColor(255, 255, 255)
+
     def __init__(self, position_x, position_y, diameter):
         self.pos_x = position_x
         self.pos_y = position_y
@@ -147,17 +195,20 @@ class Target:
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
 
+    ''' Draws the standard representation of this target to the screen'''
     def draw(self, painter):
-        painter.setBrush(QtGui.QColor(255, 255, 255))
+        painter.setBrush(self.COLOR_WHITE)
         painter.drawEllipse(QtCore.QPoint(self.pos_x, self.pos_y), self.diameter / 2, self.diameter / 2)
         return
 
+    ''' Highlight the circle with a red color'''
     def draw_highlighted(self, painter):
-        painter.setBrush(QtGui.QColor(200, 34, 20))
+        painter.setBrush(self.COLOR_RED)
         painter.drawEllipse(QtCore.QPoint(self.pos_x, self.pos_y),
-                           self.diameter / 2,
-                           self.diameter / 2)
+                            self.diameter / 2,
+                            self.diameter / 2)
 
+    ''' Draws the target to the screen with the specified color'''
     def draw_colored(self, painter, color):
         painter.setBrush(color)
         painter.drawEllipse(QtCore.QPoint(self.pos_x, self.pos_y), self.diameter / 2, self.diameter / 2)
@@ -165,17 +216,32 @@ class Target:
 
 
 class PointingExperimentTest(QtWidgets.QWidget):
+
+    """
+        Main class for executing the pointing experiment/test
+        Initializes and keeps track of the ui and pointing technique
+        and processes all mouse input events
+        Also responsible for keeping the given experiment model up to date
+
+        @param model: The experiment model used for this Test
+    """
+
     UI_WIDTH = 1920
     UI_HEIGHT = 800
     BUBBLE_RADIUS = 20
+    MAX_NUM_TARGETS = 10
+    MIN_NUM_TARGETS = 3
 
     def __init__(self, model):
         super(PointingExperimentTest, self).__init__()
         self.model = model
         self.start_pos = (self.UI_WIDTH / 2, self.UI_HEIGHT / 2)
         self.initUI()
-        self.reset_trial()
+        self.init_next_trial()
 
+    ''' Initialize the UI
+        Sets the window size, window title, focus policy and mouse startung position
+    '''
     def initUI(self):
         self.text = "Please click on the target"
         self.setGeometry(0, 0, self.UI_WIDTH, self.UI_HEIGHT)
@@ -185,7 +251,10 @@ class PointingExperimentTest(QtWidgets.QWidget):
         self.setMouseTracking(True)
         self.show()
 
-    def reset_trial(self):
+    ''' Prepares and sets all variables for the next test
+        Updates the UI and model and sets appropriate pointing technique
+    '''
+    def init_next_trial(self):
         if self.model.improve_pointing:
             self.pointing_technique = pt.PointingTechniqueFatBubble([], Target, self.BUBBLE_RADIUS)
         else:
@@ -197,14 +266,24 @@ class PointingExperimentTest(QtWidgets.QWidget):
         self.pointing_technique.update_targets(self.targets)
         self.update()
 
+    ''' Returns a randomly generated angle in Radians'''
     def getRandomAngleInRad(self):
         return math.radians(random.randint(0, 360))
 
+    ''' Initialize all targets representes to the participant
+        The target size is defined by the current trial settings
+        and the amount is set via MAX_NUM_TARGETS and MIN_NUM_TARGETS
+
+        Tries to avoid overlapping targets
+        Ignores the none overlapping restriction if the targets are too many
+        and/or too big to be placed in that manner after a few tries
+
+    '''
     def initTargets(self):
-        number_of_targets = random.randint(3, 10)
+        number_of_targets = random.randint(self.MIN_NUM_TARGETS, self.MAX_NUM_TARGETS)
         self.targets = []
         if self.model.current_trial() is not None:
-            distance, size = self.model.current_trial().getCurrentCondition()
+            distance, size = self.model.current_trial().get_current_condition()
             pos = self.getMainTargetPos(distance)
             self.targets.append(Target(pos[0], pos[1], size))
         else:
@@ -225,14 +304,19 @@ class PointingExperimentTest(QtWidgets.QWidget):
                 can_draw = not_occupied
             self.targets.append(Target(pos_x, pos_y, size))
 
+    ''' Helper for getting the center position of the target that has to be clicked'''
     def getMainTargetPos(self, distance):
         x = self.start_pos[0] + distance * math.cos(self.random_angle_in_rad)
         y = self.start_pos[1] + distance * math.sin(self.random_angle_in_rad)
         return (x, y)
 
+    ''' Helper for getting the target that has to be clicked'''
     def getMainTarget(self):
         return self.targets[0]
 
+    ''' Changes between the improved and standard pointing technique
+        Only has an effect if the improved pointing technique is allowed during the current experiment
+    '''
     def togglePointingTechnique(self):
         if not self.model.improve_pointing:
             return
@@ -241,12 +325,15 @@ class PointingExperimentTest(QtWidgets.QWidget):
         else:
             self.pointing_technique = pt.PointingTechniqueFatBubble(self.targets, Target, self.BUBBLE_RADIUS)
 
+    ''' Processes all click events for the mouse
+        and checks if the main target has been hit
+    '''
     def mousePressEvent(self, ev):
         if ev.button() == QtCore.Qt.LeftButton:
             main_target = self.getMainTarget()
             if main_target in self.pointing_technique.get_targets_under_cursor():
                 self.model.register_click([main_target.pos_x, main_target.pos_y], [ev.x(), ev.y()])
-                self.reset_trial()
+                self.init_next_trial()
             else:
                 self.model.increment_error_count(1)
                 return
@@ -255,6 +342,7 @@ class PointingExperimentTest(QtWidgets.QWidget):
             self.pointing_technique.filter(ev.x(), ev.y())
         self.update()
 
+    ''' Processes all movement events for the mouse and reroutes them to the pointing technique'''
     def mouseMoveEvent(self, ev):
         self.pointing_technique.filter(ev.x(), ev.y())
         if (abs(ev.x() - self.start_pos[0]) > 5) or (abs(ev.y() - self.start_pos[1]) > 5):
@@ -262,6 +350,7 @@ class PointingExperimentTest(QtWidgets.QWidget):
         self.update()
         return
 
+    ''' Draws all elements to the screen'''
     def paintEvent(self, event):
         qp = QtGui.QPainter()
         qp.begin(self)
@@ -270,12 +359,14 @@ class PointingExperimentTest(QtWidgets.QWidget):
         self.pointing_technique.draw_pointer(qp)
         qp.end()
 
+    ''' Draws a text showing the passed time for this test'''
     def drawText(self, event, qp):
         qp.setPen(QtGui.QColor(168, 34, 3))
         qp.setFont(QtGui.QFont('Decorative', 32))
         self.text = "%d / %d (%05d ms)" % (self.model.elapsed, len(self.model.trials), self.model.timer.elapsed())
         qp.drawText(event.rect(), QtCore.Qt.AlignTop, self.text)
 
+    ''' Responsible for drawing all targets depending on their current state (hovered, main target, normal target)'''
     def drawTargets(self, qp):
         for idx in range(len(self.targets)):
             if idx == 0:
@@ -288,6 +379,15 @@ class PointingExperimentTest(QtWidgets.QWidget):
 
 
 def main():
+
+    """
+        Starting class for this script
+        Reads in a ini or json file passed as a command line parameter (see definition above)
+        and initializes the experiment model with this information
+        Starts the QtApplication afterwards with the newly created model
+
+    """
+
     app = QtWidgets.QApplication(sys.argv)
     if len(sys.argv) < 2:
         sys.stderr.write("Usage: %s <setup file>\n" % sys.argv[0])
@@ -302,6 +402,14 @@ def main():
 
 
 def parse_ini_file(filename):
+
+    """
+        Reads the information from a ini file
+
+        @return: Integer with the user's id; a list with all possible target sizes for this test;
+                a list with all possible target distances for this test; Boolean indicating whether the improved
+                pointing technique should be used or not
+    """
     config = configparser.ConfigParser()
     config.read(filename)
     if 'experiment_setup' in config:
@@ -319,6 +427,14 @@ def parse_ini_file(filename):
 
 
 def parse_json_file(filename):
+
+    """
+        Reads the information from a json file
+
+        @return: Integer with the user's id; a list with all possible target sizes for this test;
+                 a list with all possible target distances for this test; Boolean indicating whether the improved
+                 pointing technique should be used or not
+    """
     setup = json.load(open(filename))
     if "UserID" not in setup or "Widths" not in setup or "Distances" not in setup:
         print("Error: wrong file format.")
--- pointing_technique.py
+++ pointing_technique.py
@@ -4,6 +4,8 @@
 import math
 from PyQt5 import QtGui, QtCore
 
+# This script was created by Alexander Frummet and Marco Batzdorf
+
 
 class StandardPointingTechnique(object):
 
@@ -19,12 +21,13 @@ class StandardPointingTechnique(object):
     """
 
     targets = []
+
     def __init__(self, targets, Target):
         self.targets = targets
         self.target_class = Target
 
     ''' Filtering, storing and manipulating mouse move events
-        
+
         @param pos_x: New mouse position on the X-Axis
         @param pos_y: New mouse position on the Y-Axis
     '''
@@ -33,7 +36,7 @@ class StandardPointingTechnique(object):
         self.cursor_pos_y = pos_y
 
     ''' Draws the pointer's visual representation to the screen
-    
+
         @param painter: PyQt QPainter object that can draw to the canvas
     '''
     def draw_pointer(self, painter):
@@ -41,19 +44,20 @@ class StandardPointingTechnique(object):
 
     ''' Checking which of the given targets is currently under
         the pointer's clickable area
-        
+
         @return: A list of all targets that are currently under the pointer
     '''
     def get_targets_under_cursor(self):
 
         hits = []
         for target in self.targets:
-            if GeometryUtils.is_point_inside_circle([self.cursor_pos_x, self.cursor_pos_y],[target.pos_x, target.pos_y],target.diameter):
+            if GeometryUtils.is_point_inside_circle([self.cursor_pos_x, self.cursor_pos_y],
+                                                    [target.pos_x, target.pos_y], target.diameter):
                 hits.append(target)
         return hits
 
     ''' Helper class to replace the current targets with new ones
-    
+
         @param targets: The new list of targets replacing the old target list
     '''
     def update_targets(self, targets):
@@ -75,11 +79,11 @@ class PointingTechniqueFatBubble(StandardPointingTechnique):
 
     COLOR_BLUE = QtGui.QColor(0, 0, 255)
 
-    def __init__(self, targets, Target, bubble_radius = 20):
+    def __init__(self, targets, Target, bubble_radius=20):
         super().__init__(targets, Target)
         self.cursor_area_radius = bubble_radius
 
-    ''' In addition to the system's standard pointer representation a blue circle 
+    ''' In addition to the system's standard pointer representation a blue circle
         is drawn around the center
 
         @param painter: PyQt QPainter object that can draw to the canvas
@@ -87,17 +91,21 @@ class PointingTechniqueFatBubble(StandardPointingTechnique):
     def draw_pointer(self, painter):
         painter.setBrush(self.COLOR_BLUE)
         painter.drawEllipse(QtCore.QPoint(self.cursor_pos_x, self.cursor_pos_y), self.cursor_area_radius,
-                       self.cursor_area_radius)
+                            self.cursor_area_radius)
 
     ''' Checking which of the given targets is currently under
         the pointer's clickable area
-        
+
         @return: A list of all targets that are currently under the pointer
     '''
     def get_targets_under_cursor(self):
         hits = []
         for target in self.targets:
-            if GeometryUtils.are_circles_intersecting(self.cursor_pos_x, self.cursor_pos_y, self.cursor_area_radius, target.pos_x, target.pos_y, target.diameter / 2):
+            if GeometryUtils.are_circles_intersecting(self.cursor_pos_x,
+                                                      self.cursor_pos_y,
+                                                      self.cursor_area_radius,
+                                                      target.pos_x, target.pos_y,
+                                                      target.diameter / 2):
                 hits.append(target)
         return hits
 
@@ -109,10 +117,10 @@ class GeometryUtils:
     """
 
     ''' Calculates the length of the vector between two points
-        
+
         @param point1: Vector containing x- and y-coordinates defining a point in 2D space
         @param point2: Vector containing x- and y-coordinates defining a another point in 2D space#
-        
+
         @return: The distance between the points
     '''
     @staticmethod
@@ -120,14 +128,14 @@ class GeometryUtils:
         return math.sqrt(math.pow(point2[0] - point1[0], 2) + math.pow(point2[1] - point1[1], 2))
 
     ''' Checks if two circles are colliding with each other
-        
+
         @param x1: X-Coordinate of the first circle
         @param y1: Y-Coordinate of the first circle
         @param radius1: Radius of the first circle
         @param x2: X-Coordinate of the second circle
         @param y2: Y-Coordinate of the second circle
         @param radius2: Radius of the second circle
-        
+
         @return: True if the two circles are intersecting, False if not
     '''
     @staticmethod
@@ -137,11 +145,11 @@ class GeometryUtils:
 
     ''' Calculates the distance between the point and circle's center point
         the point is inside if the distance is even or smaller than the circle's radius
-        
+
         @param point: Vector containing x- and y-coordinates defining a point in 2D space
         @param center: Vector containing x- and y-coordinates defining a point in 2D space
         @param diameter: The diameter defining the extends of the circle
-        
+
         @return: True if the point is inside the given circle, False if not
     '''
     @staticmethod
